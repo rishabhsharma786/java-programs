4539fcf15e2d78ffb8e2644e69d5e6f814320f66
Rishabh Sharma <43056968+rishabhsharma786@users.noreply.github.com>
2020-02-28 21:38:09 +0530

    Area of circle

---
+++ Area.java
@@ -0,0 +1,13 @@
+//Area of circle
+import java.util.*;
+class Area
+{
+	public static void main(String ar[])
+	{
+		double pi,a,r;
+		pi=3.14;
+		r=4;
+		a=pi*r*r;
+		System.out.println("Area of circle:="+a);
+	}
+}
